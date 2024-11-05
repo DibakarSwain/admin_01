07e4ef61207002cda32ed6426dadef3d39b46e33
DibakarSwain <144449715+DibakarSwain@users.noreply.github.com>
2024-11-05 22:51:53 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -36,7 +36,7 @@ export const Register = () => {
     console.log(user);
 
     try {
-      const response = await fetch("https://admin-01.onrender.com/api/form/contact/api/auth/login/api/auth/register", {
+      const response = await fetch("https://admin-01.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
