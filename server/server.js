4d38418fb420ccc61900d487c62f66f196fcb23b
DibakarSwain <144449715+DibakarSwain@users.noreply.github.com>
2024-11-05 23:49:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const path = require("path");
 
 const authRoute = require("./router/auth-router");
